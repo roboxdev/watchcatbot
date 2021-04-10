29dc9b18bbd0f04927f6a9e054b266e051c59ad6
Maxim Kolesnikov <root@roboxv.pro>
2021-04-10 18:27:32 +0600

    Make controller more robust: auto-reconnect to wifi and blynk server; Use `timer` lib instead of counting `millis`

--- door_controller/door_controller.ino
+++ door_controller/door_controller.ino
@@ -1,5 +1,8 @@
 #define BLYNK_PRINT Serial
 
+#define BLYNK_TIMEOUT_MS  500  // must be BEFORE BlynkSimpleEsp8266.h doesn't work !!!
+#define BLYNK_HEARTBEAT   17   // must be BEFORE BlynkSimpleEsp8266.h works OK as 17s
+
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
@@ -10,7 +13,6 @@
 #define BLYNK_COUNTDOWN_PIN V3
 #define BUTTON_PIN 13
 #define OPTOCOUPLER_PIN 15
-#define LED_PIN 2
 #define DEFAULT_DURATION 20
 
 // You should get Auth Token in the Blynk App.
@@ -22,30 +24,40 @@ char auth[] = SECRET_AUTH_TOKEN;
 char ssid[] = SECRET_SSID;
 char pass[] = SECRET_PASS;
 
-unsigned long holdingDoorSince = 0;
-int countdown = 0;
-int doorOpeningDuration = 0;
+BlynkTimer timer;
 
-void setDoorState(bool doorState) {
-  Blynk.virtualWrite(BLYNK_DOOR_STATE_PIN, doorState);
-  digitalWrite(LED_PIN, doorState);
-  digitalWrite(OPTOCOUPLER_PIN, doorState);
-}
+bool on = false;
+bool online = false;
+
+int countdown = 0;
+int countdownTimerId;
 
 void closeDoor() {
   Serial.println("close a door");
   
+  if (timer.isEnabled(countdownTimerId)) {
+    timer.deleteTimer(countdownTimerId);
+  }
+  
   Blynk.virtualWrite(BLYNK_COUNTDOWN_PIN, 0);
   Blynk.virtualWrite(BLYNK_INPUT_PIN, 0);
   
-  holdingDoorSince = 0;
   countdown = 0;
-  doorOpeningDuration = 0;
   
-  setDoorState(false); 
+  Blynk.virtualWrite(BLYNK_DOOR_STATE_PIN, false);
+  digitalWrite(LED_BUILTIN, false);
+  digitalWrite(OPTOCOUPLER_PIN, false);
 }
 
-void openDoorFor(int duration) {
+void updateCountdown() {
+  countdown--;
+  Serial.print("countdown: ");
+  Serial.println(countdown);
+  Blynk.virtualWrite(BLYNK_COUNTDOWN_PIN, countdown);
+  
+}
+
+void openDoorFor(int duration = DEFAULT_DURATION) {
   closeDoor();
   
   Serial.print("open a door for: ");
@@ -54,25 +66,16 @@ void openDoorFor(int duration) {
   Blynk.virtualWrite(BLYNK_COUNTDOWN_PIN, duration);
   Blynk.virtualWrite(BLYNK_INPUT_PIN, duration);
   
-  holdingDoorSince = millis();
   countdown = duration;
-  doorOpeningDuration = duration;
   
-  setDoorState(true); 
+  Blynk.virtualWrite(BLYNK_DOOR_STATE_PIN, true);
+  digitalWrite(LED_BUILTIN, true);
+  digitalWrite(OPTOCOUPLER_PIN, true);
+
+  timer.setTimeout(duration * 1000L, closeDoor);
+  countdownTimerId = timer.setTimer(1000, updateCountdown, duration);
 }
 
-void updateCountdown() {
-  int newCountdown = doorOpeningDuration - ((millis() - holdingDoorSince) / 1000);
-  if (newCountdown <= 0) {
-    closeDoor();
-  }
-  if (newCountdown != countdown) {
-    Serial.print("countdown: ");
-    Serial.println(newCountdown);
-    Blynk.virtualWrite(BLYNK_COUNTDOWN_PIN, newCountdown);
-    countdown = newCountdown;
-  }
-}
 
 BLYNK_WRITE(BLYNK_INPUT_PIN) {
   int pinValue = param.asInt(); // assigning incoming value from pin to a variable
@@ -86,23 +89,64 @@ BLYNK_WRITE(BLYNK_INPUT_PIN) {
 void setup() {
   pinMode(BUTTON_PIN, INPUT_PULLUP);
   pinMode(OPTOCOUPLER_PIN, OUTPUT);
-  pinMode(LED_PIN, OUTPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
   
   // Debug console
   Serial.begin(115200);
   Serial.println("setup");
-  
-  Blynk.begin(auth, ssid, pass);
 
-  openDoorFor(DEFAULT_DURATION);
+  // WiFi
+  WiFi.hostname(WIFI_HOSTNAME);
+  WiFi.mode(WIFI_STA);
+
+  // Blynk
+  Blynk.config(auth);
+
+  checkConnection(); // It needs to run first to initiate the connection.
+  //Same function works for checking the connection!
+  timer.setInterval(5000L, checkConnection);
 }
 
 void loop() {
-  if (countdown) {
-    updateCountdown();    
+  if (Blynk.connected()) {
+    Blynk.run();
   }
+  timer.run();
+  
   if (!digitalRead(BUTTON_PIN) && countdown < DEFAULT_DURATION) {
     openDoorFor(DEFAULT_DURATION);
   }
-  Blynk.run();
+}
+
+void checkConnection() {
+  if (!Blynk.connected()) {
+    online = 0;
+    yield();
+    if (WiFi.status() != WL_CONNECTED) {
+      Serial.println("Not connected to Wifi! Connect...");
+      Blynk.connectWiFi(ssid, pass);
+      delay(400); //give it some time to connect
+      if (WiFi.status() != WL_CONNECTED) {
+        Serial.println("Cannot connect to WIFI!");
+        online = 0;
+      } else {
+        Serial.println("Connected to wifi!");
+      }
+    }
+    
+    if ( WiFi.status() == WL_CONNECTED && !Blynk.connected() ) {
+      Serial.println("Not connected to Blynk Server! Connecting..."); 
+      Blynk.connect();  // // It has 3 attempts of the defined BLYNK_TIMEOUT_MS to connect 
+      // to the server, otherwise it goes to the enxt line 
+      if (!Blynk.connected()) {
+        Serial.println("Connection failed!"); 
+        online = 0;
+      } else {
+        online = 1;
+      }
+    }
+  } else {
+    Serial.println("Connected to Blynk server!"); 
+    online = 1;    
+  }
 }
--- door_controller/secrets.example.h
+++ door_controller/secrets.example.h
@@ -4,3 +4,4 @@
 #define SECRET_SSID "my_ssid"
 #define SECRET_PASS "my_password"
 #define SECRET_AUTH_TOKEN "my_auth_token"
+#define WIFI_HOSTNAME "my_hostname"
